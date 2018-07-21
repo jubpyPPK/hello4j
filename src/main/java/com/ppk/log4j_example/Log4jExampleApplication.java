7ce19cdb05a290519b3ff62ebba419b383f20211
Prapakorn <prapakorn.jar@ascendcorp.com>
2018-07-21 16:07:15 +0700

    Initial project and add example.

---
+++ src/main/java/com/ppk/log4j_example/Log4jExampleApplication.java
@@ -0,0 +1,12 @@
+package com.ppk.log4j_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Log4jExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Log4jExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ppk/log4j_example/Log4jExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ppk.log4j_example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Log4jExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
